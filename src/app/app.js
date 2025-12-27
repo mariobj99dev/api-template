34d8152fb434040f0b03bc34297a1bb87e334e68
Mario Bellido Jiménez <mariobj1999@gmail.com>
2025-12-27 17:43:22 +0100

    feat: Added helmet, cors, express-rate-limit

--- src/app/app.js
+++ src/app/app.js
@@ -1,20 +1,41 @@
 
-const express = require('express')
+const express = require('express');
 const cookieParser = require('cookie-parser');
-const helmet = require('helmet')
-const rateLimit = require('express-rate-limit')
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cors = require('cors');
 
-const routes = require('./routes')
-const errorMiddleware = require('./middlewares/error.middleware')
+const routes = require('./routes');
+const errorMiddleware = require('./middlewares/error.middleware');
 
 const apiLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
     max: 100,
 });
 
+const corsOptions = {
+    origin: (origin, callback) => {
+        const allowedOrigins = [
+            'http://localhost:3000',
+            'http://localhost:5173',
+        ];
+
+        // Permitir requests sin origin (Postman, curl)
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.includes(origin)) {
+            return callback(null, true);
+        }
+
+        return callback(new Error('Not allowed by CORS'));
+    },
+    credentials: true,
+};
+
 const app = express()
-app.use(apiLimiter)
-app.use(helmet())
+app.use(cors(corsOptions));
+app.use(apiLimiter);
+app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,4 +43,4 @@ routes(app);
 
 app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
